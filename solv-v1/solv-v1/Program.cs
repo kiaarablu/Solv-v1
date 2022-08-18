b4d75843729c9ebec4afbded1fc76337b7695658
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-18 14:20:03 +0430

    Add files via upload

---
+++ solv-v1/solv-v1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solv_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process a = new Process();
+            a.AddFirst(1);
+            a.AddLast(2);
+            a.AddLast(3);
+            a.AddLast(4);
+            a.AddLast(5);
+            a.AddLast(6);
+            a.printsolv();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ solv-v1/solv-v1/Solv.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solv_v1
+{
+
+
+
+    public class Box
+    {
+        public int Value;
+        public Box Address;
+    }
+    public class Process
+    {
+        public bool BoxIsNull()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        Box head = null;
+        public void AddFirst(int meghdar)
+        {
+
+            if (BoxIsNull())
+            {
+                Box first = new Box();
+                head = first;
+                head.Value = meghdar;
+                head.Address = null;
+            }
+            else
+            {
+                Box secound = new Box();
+                secound.Value = meghdar;
+                secound.Address = head;
+                head = secound;
+
+            }
+
+        }
+        public void AddLast(int val)
+        {
+            if (BoxIsNull())
+            {
+
+                Box last = new Box();
+                last.Value = val;
+                last.Address = null;
+                head = last;
+            }
+            else
+            {
+                Box box = new Box();
+                box.Value = val;
+                Box box1 = head;
+
+                while (box1.Address != null)
+                {
+                    box1 = box1.Address;
+                }
+                box1.Address = box;
+                box.Value = val;
+            }
+        }
+        public void Solve(Box start)
+        {
+            if (start == null)
+            {
+                return;
+            }
+            Console.WriteLine(start.Value);
+            if (start.Address != null)
+            {
+                Solve(start.Address.Address);
+            }
+
+            Console.WriteLine(start.Value);
+        }
+        public void printsolv()
+        {
+            Solve(head);
+        }
+
+        public void Print()
+        {
+            var crnt = head;
+            while (crnt != null)
+            {
+                Console.WriteLine(crnt.Value);
+                crnt = crnt.Address;
+            }
+
+        }
+
+
+    }
+}
+
+
+
